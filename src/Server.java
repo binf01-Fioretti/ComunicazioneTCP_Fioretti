dff87e38984d29f755f713b266365edff113f0c9
Fioretti Daniele <fiorettidaniele9@gmail.com>
2025-02-04 13:34:04 +0100

    Aggiunto metodo Server e attendi

--- src/Server.java
+++ src/Server.java
@@ -1,2 +1,27 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    ServerSocket serverSocket;
+    Socket clientSocket;
+    int porta;
+    public Server(int porta){
+        this.porta = porta;
+        try {
+            serverSocket = new ServerSocket(porta);
+            System.out.println("Il server è in ascolto");
+        } catch (IOException e) {
+            System.err.println("Errore nella fase di ascolto");
+        }
+    }
+    public Socket attendi (){
+        try {
+            clientSocket = serverSocket.accept();
+        } catch (IOException e) {
+            System.err.println("Problemi di connessione con il client");
+        }
+        return clientSocket;
+    }
+
 }
